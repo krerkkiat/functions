6b01c0179c1d557bda63e5e583ddf8da788fb348
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2021-04-11 23:09:29 -0400

    feat: add link to an appointment website

---
+++ README.md
@@ -0,0 +1,7 @@
+# Collection of Cloud Functions
+
+## Deploying
+
+``` console
+$ gcloud functions deploy func-name --runtime nodejs12 --trigger-http --allow-unauthenticated
+```
--- when-to-get-covid-test/index.js
+++ when-to-get-covid-test/index.js
@@ -9,13 +9,13 @@ exports.whenToGetCovidTest = (req, res) => {
 
     if (type === 'ak') {
         if (numberOfWeeks % 2 === 0) {
-            res.send('You need to get tested this week.')
+            res.send('You need to get tested this week. <a href="https://app.acuityscheduling.com/schedule.php?owner=21518306" target="_blank" rel="noopener noreferrer">Schedule the test (Athens campus)</a>')
         } else {
             res.send('You need to get tested next week.')
         }
     } else if (type === 'lz') {
         if (numberOfWeeks % 2 === 1) {
-            res.send('You need to get tested this week.')
+            res.send('You need to get tested this week. <a href="https://app.acuityscheduling.com/schedule.php?owner=21518306" target="_blank" rel="noopener noreferrer">Schedule the test (Athens campus)</a>')
         } else {
             res.send('You need to get tested next week.')
         }
